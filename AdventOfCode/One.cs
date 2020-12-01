3609c1f928365969d93a05794e4ad464dac74221
Ollie Philpott <ophilpott@zonedigital.com>
2020-12-01 13:27:15 +0000

    Day one part 2

--- AdventOfCode/One.cs
+++ AdventOfCode/One.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,19 +15,53 @@ namespace AdventOfCode
             var lines = text.Split(Environment.NewLine);
             var dictionary = lines.ToDictionary(int.Parse, int.Parse);
 
+            RunPart1(dictionary);
+            RunPart2(dictionary);
+        }
+
+        private static void RunPart1(Dictionary<int, int> dictionary)
+        {
+            var numbers = FindNumberForSum(dictionary, 2020);
+            if (numbers != null)
+            {
+                Console.WriteLine($"Found a match: {numbers.Value.Item1} and {numbers.Value.Item2}");
+                Console.WriteLine($"Sum is {numbers.Value.Item1 * numbers.Value.Item2}");
+                return;
+            }
+
+            Console.WriteLine("No matches found");
+        }
+
+        private static void RunPart2(Dictionary<int, int> dictionary)
+        {
             foreach (var item in dictionary)
             {
-                var otherNumber = 2020 - item.Value;
-
-                if (dictionary.ContainsKey(otherNumber))
+                var subSum = 2020 - item.Value;
+                var numbers = FindNumberForSum(dictionary, subSum);
+                if (numbers != null)
                 {
-                    Console.WriteLine($"Found a match: {item.Value} and {otherNumber}");
-                    Console.WriteLine($"Sum is {item.Value * otherNumber}");
+                    Console.WriteLine($"Found a match: {item.Value} and {numbers.Value.Item1} and {numbers.Value.Item2}");
+                    Console.WriteLine($"Sum is {item.Value * numbers.Value.Item1 * numbers.Value.Item2}");
                     return;
                 }
             }
 
             Console.WriteLine("No matches found");
+        }
+
+        private static (int, int)? FindNumberForSum(Dictionary<int, int> dictionary, int sum)
+        {
+            foreach (var item in dictionary)
+            {
+                var otherNumber = sum - item.Value;
+
+                if (dictionary.ContainsKey(otherNumber))
+                {
+                    return (item.Value, otherNumber);
+                }
+            }
+
+            return null;
         }
     }
 }
