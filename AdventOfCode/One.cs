2148db17d23de11b8f8150d2704c39df088d105d
Ollie Philpott <ophilpott@zonedigital.com>
2020-12-01 13:13:02 +0000

    First puzzle

---
+++ AdventOfCode/One.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class One
+    {
+        public static async Task Run()
+        {
+            using var file = File.OpenText($"{AppContext.BaseDirectory}/1.txt");
+            var text = await file.ReadToEndAsync();
+            var lines = text.Split(Environment.NewLine);
+            var dictionary = lines.ToDictionary(int.Parse, int.Parse);
+
+            foreach (var item in dictionary)
+            {
+                var otherNumber = 2020 - item.Value;
+
+                if (dictionary.ContainsKey(otherNumber))
+                {
+                    Console.WriteLine($"Found a match: {item.Value} and {otherNumber}");
+                    Console.WriteLine($"Sum is {item.Value * otherNumber}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("No matches found");
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await One.Run();
+        }
+    }
+}
