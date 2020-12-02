cfd2073ab8c3c5e9c26cb6f9bbc49a4b28b014bf
Ollie Philpott <ophilpott@zonedigital.com>
2020-12-02 10:39:04 +0000

    Day 2 part 2

--- AdventOfCode/Challenges/Two.cs
+++ AdventOfCode/Challenges/Two.cs
@@ -10,34 +10,81 @@ namespace AdventOfCode.Challenges
         public static async Task Run()
         {
             var lines = await FileHelper.ReadLines("2.txt");
+            RunPart1(lines);
+            RunPart2(lines);
+        }
+
+        private static void RunPart1(string[] lines)
+        {
             var validCount =
-                lines.Select(line => new PasswordRule(line))
+                lines.Select(line => new Part1PasswordRule(line))
                     .Count(rule => rule.IsValid());
 
-            Console.WriteLine($"{validCount} valid passwords");
+            Console.WriteLine($"{validCount} valid passwords for part 1");
+        }
+
+        private static void RunPart2(string[] lines)
+        {
+            var validCount =
+                lines.Select(line => new Part2PasswordRule(line))
+                    .Count(rule => rule.IsValid());
+
+            Console.WriteLine($"{validCount} valid passwords for part 2");
         }
 
         public class PasswordRule
         {
-            public PasswordRule(string rule)
+            protected PasswordRule(string rule)
             {
                 var tokens = rule.Split(" ");
                 var ruleTokens = tokens[0].Split("-");
-                LowerLimit = int.Parse(ruleTokens[0]);
-                UpperLimit = int.Parse(ruleTokens[1]);
+                FirstNumber = int.Parse(ruleTokens[0]);
+                SecondNumber = int.Parse(ruleTokens[1]);
                 Letter = tokens[1][0];
                 Password = tokens[2];
             }
 
-            public int LowerLimit { get; set; }
-            public int UpperLimit { get; set; }
+            public int FirstNumber { get; set; }
+            public int SecondNumber { get; set; }
             public char Letter { get; set; }
             public string Password { get; set; }
+        }
 
-            public bool IsValid()
+        public class Part1PasswordRule : PasswordRule
+        {
+            public Part1PasswordRule(string rule)
+                : base(rule)
+            {
+            }
+
+            public virtual bool IsValid()
             {
                 var letterCount = Password.Count(c => c == Letter);
-                return letterCount >= LowerLimit && letterCount <= UpperLimit;
+                return letterCount >= FirstNumber && letterCount <= SecondNumber;
+            }
+        }
+
+        public class Part2PasswordRule : PasswordRule
+        {
+            public Part2PasswordRule(string rule)
+                : base(rule)
+            {
+            }
+
+            public virtual bool IsValid()
+            {
+                var letterCount = 0;
+                if (Password.ElementAt(FirstNumber - 1) == Letter)
+                {
+                    letterCount++;
+                }
+
+                if (Password.ElementAt(SecondNumber - 1) == Letter)
+                {
+                    letterCount++;
+                }
+
+                return letterCount == 1;
             }
         }
     }
