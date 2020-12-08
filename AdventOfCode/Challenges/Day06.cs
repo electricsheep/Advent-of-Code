b9d570dc7cb8f6d36ea77015740875818f29ca64
Ollie Philpott <ophilpott@zonedigital.com>
2020-12-08 14:56:30 +0000

    Day 6 part 2

--- AdventOfCode/Challenges/Day06.cs
+++ AdventOfCode/Challenges/Day06.cs
@@ -10,17 +10,41 @@ namespace AdventOfCode.Challenges
         public static void Run()
         {
             var text = File.ReadAllText($"{AppContext.BaseDirectory}/6.txt");
-            var groups = text.Split($"{Environment.NewLine}{Environment.NewLine}").Select(x => x.Replace($"{Environment.NewLine}", ""));
+            var groups = text.Split($"{Environment.NewLine}{Environment.NewLine}");
             RunPart1(groups);
+            RunPart2(groups);
         }
 
-        public static void RunPart1(IEnumerable<string>groups)
+        public static void RunPart1(IEnumerable<string> groups)
         {
             var answerCounts = new Dictionary<char, int>();
 
             foreach (var group in groups)
             {
-                foreach (var letter in group.Distinct())
+                foreach (var letter in group.Replace($"{Environment.NewLine}", "").Distinct())
+                {
+                    if (!answerCounts.ContainsKey(letter))
+                    {
+                        answerCounts.Add(letter, 0);
+                    }
+                    answerCounts[letter]++;
+                }
+            }
+
+            Console.WriteLine($"Sum: {answerCounts.Values.Aggregate(0, (x, y) => x + y)}");
+        }
+
+        public static void RunPart2(IEnumerable<string> groups)
+        {
+            var answerCounts = new Dictionary<char, int>();
+
+            foreach (var group in groups)
+            {
+                var groupSize = group.Split($"{Environment.NewLine}").Length;
+                var groupedLetters = group.Replace($"{Environment.NewLine}", "").GroupBy(x => x);
+                var allAnswered = groupedLetters.Where(x => x.Count() == groupSize).Select(x => x.Key);
+
+                foreach (var letter in allAnswered)
                 {
                     if (!answerCounts.ContainsKey(letter))
                     {
