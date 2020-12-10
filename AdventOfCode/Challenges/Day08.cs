40734f33f1f6169f7b1d1649011cf86f20cc534e
Ollie Philpott <ophilpott@zonedigital.com>
2020-12-10 16:39:19 +0000

    Day 8 part 2

--- AdventOfCode/Challenges/Day08.cs
+++ AdventOfCode/Challenges/Day08.cs
@@ -12,6 +12,7 @@ namespace AdventOfCode.Challenges
         {
             var instructions = File.ReadAllLines($"{AppContext.BaseDirectory}/8.txt");
             RunPart1(instructions);
+            RunPart2(instructions);
         }
 
         public static void RunPart1(string[] instructions)
@@ -19,6 +20,7 @@ namespace AdventOfCode.Challenges
             var executedInstructions = new List<int>();
             var nextInstructionIndex = 0;
             var acc = 0;
+
             while (!executedInstructions.Contains(nextInstructionIndex))
             {
                 executedInstructions.Add(nextInstructionIndex);
@@ -41,6 +43,59 @@ namespace AdventOfCode.Challenges
             }
 
             Console.WriteLine($"Acc: {acc}");
+        }
+
+        public static void RunPart2(string[] instructions)
+        {
+            foreach (var nopInstruction in instructions.Where(x => x.Contains("nop")))
+            {
+                RunProgram(instructions, nopInstruction, "nop", "jmp");
+            }
+
+            foreach (var jmpInstruction in instructions.Where(x => x.Contains("jmp")))
+            {
+                RunProgram(instructions, jmpInstruction, "jmp", "nop");
+            }
+        }
+
+        private static void RunProgram(string[] instructions, string instructionToReplace, string instructionFrom, string instructionTo)
+        {
+            var executedInstructions = new List<int>();
+            var nextInstructionIndex = 0;
+            var acc = 0;
+
+            while (!executedInstructions.Contains(nextInstructionIndex))
+            {
+                if (nextInstructionIndex == instructions.Length - 1)
+                {
+                    Console.WriteLine("Program complete!");
+                    Console.WriteLine($"Acc: {acc}");
+                    return;
+                }
+
+                executedInstructions.Add(nextInstructionIndex);
+                var instruction = instructions[nextInstructionIndex];
+                if (instruction == instructionToReplace)
+                {
+                    instruction = instruction.Replace(instructionFrom, instructionTo);
+                }
+
+                var parts = instruction.Split(" ");
+
+                switch (parts[0])
+                {
+                    case "acc":
+                        acc += ParseNumber(parts[1]);
+                        nextInstructionIndex++;
+                        break;
+                    case "nop":
+                        nextInstructionIndex++;
+                        break;
+                    case "jmp":
+                        nextInstructionIndex += ParseNumber(parts[1]);
+                        break;
+                }
+            }
         }
 
         private static int ParseNumber(string signedNumber)
